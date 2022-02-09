a04c5672a06959aa86fba77d2b98cddfe9ff6b79
oBielov <i.byelov@gmail.com>
2022-02-09 14:55:10 +0200

    initCommit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,31 +1,132 @@
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.SneakyThrows;
-import models.JsonModel;
-import org.json.simple.JSONObject;
 
-import java.util.Map;
-import java.util.Set;
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  *
  */
 public class Parser {
 
-    @SneakyThrows
-    public JsonNode parse(JSONObject before, JSONObject after) {
+    private final ObjectMapper mapper = new ObjectMapper();
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode leftTree = mapper.readTree(before.toJSONString());
-        JsonNode rightTree = mapper.readTree(after.toJSONString());
-        return leftTree;
+    @SneakyThrows
+    public JsonNode parse(JsonNode before, JsonNode after) {
+        ObjectNode node = mapper.createObjectNode();
+        node.put("meta", compareMeta(before, after));
+        node.put("candidates", compareCandidates(before, after));
+        return node;
     }
+
+    @SneakyThrows
+    private ArrayNode compareMeta(JsonNode before, JsonNode after){
+        Map<String, Object> beforeMeta = mapper.convertValue(before.get("meta")
+                , new TypeReference<Map<String, Object>>(){});
+        Map<String, Object> afterMeta = mapper.convertValue(after.get("meta")
+                , new TypeReference<Map<String, Object>>(){});
+        ArrayNode arrayNode = mapper.createArrayNode();
+
+        for(String key : beforeMeta.keySet()){
+            Object a = beforeMeta.get(key);
+            Object b = afterMeta.get(key);
+            if(!a.equals(b)){
+                ObjectNode object = mapper.createObjectNode();
+                object.put("field", key)
+                        .put("before", a.toString())
+                        .put("after", b.toString());
+                arrayNode.add(object);
+            }
+        }
+        return arrayNode;
+    }
+
+    @SneakyThrows
+    private JsonNode compareCandidates(JsonNode before, JsonNode after){
+        ObjectNode node = mapper.createObjectNode();
+        node.put("edited", compareCommon(before, after));
+        node.put("added", findAdded(before, after));
+        node.put("removed", findRemoved(before, after));
+        return node;
+    }
+
+    @SneakyThrows
+    private JsonNode findAdded(JsonNode before, JsonNode after){
+        ArrayNode array = mapper.createArrayNode();
+        JsonNode beforeC = mapper.readTree(before.get("candidates").toPrettyString());
+        JsonNode afterC = mapper.readTree(after.get("candidates").toPrettyString());
+        List<JsonNode> beforeIds = beforeC.findValues("id");
+        List<JsonNode> afterIds = afterC.findValues("id");
+        List<JsonNode> added = afterIds.stream()
+                .filter(id -> !beforeIds.contains(id))
+                .collect(Collectors.toList());
+        for(JsonNode node : added){
+            ObjectNode obj = mapper.createObjectNode();
+            obj.put("id", node.toPrettyString());
+            array.add(obj);
+        }
+        return array;
+    }
+
+    @SneakyThrows
+    private JsonNode findRemoved(JsonNode before, JsonNode after){
+        ArrayNode array = mapper.createArrayNode();
+        JsonNode beforeC = mapper.readTree(before.get("candidates").toPrettyString());
+        JsonNode afterC = mapper.readTree(after.get("candidates").toPrettyString());
+        List<JsonNode> beforeIds = beforeC.findValues("id");
+        List<JsonNode> afterIds = afterC.findValues("id");
+        List<JsonNode> removed = beforeIds.stream()
+                .filter(id -> !afterIds.contains(id))
+                .collect(Collectors.toList());
+        for(JsonNode node : removed){
+            ObjectNode obj = mapper.createObjectNode();
+            obj.put("id", node.toPrettyString());
+            array.add(obj);
+        }
+        return array;
+    }
+
+    @SneakyThrows
+    private JsonNode compareCommon(JsonNode before, JsonNode after) {
+        ArrayNode array = mapper.createArrayNode();
+        JsonNode beforeC = mapper.readTree(before.get("candidates").toPrettyString());
+        JsonNode afterC = mapper.readTree(after.get("candidates").toPrettyString());
+        List<JsonNode> beforeIds = beforeC.findValues("id");
+        List<JsonNode> afterIds = afterC.findValues("id");
+        List<String> common = beforeIds.stream()
+                .filter(afterIds::contains)
+                .map(JsonNode::toString)
+                .collect(Collectors.toList());
+        List<JsonNode> beforeList = new ArrayList<>();
+        List<JsonNode> afterList = new ArrayList<>();
+        for (String s : common){
+            for (JsonNode n : beforeC){
+                if(n.findValue("id").toString().equals(s)){
+                    beforeList.add(n);
+                }
+            }
+            for (JsonNode n : afterC){
+                if(n.findValue("id").toString().equals(s)){
+                    afterList.add(n);
+                }
+            }
+        }
+        for(int i = 0; i < beforeList.size(); i++){
+            JsonNode b = beforeList.get(i);
+            JsonNode a = afterList.get(i);
+            if(!b.equals(a)){
+                ObjectNode obj = mapper.createObjectNode();
+                obj.put("id", b.findValue("id").toPrettyString());
+                array.add(obj);
+            }
+        }
+        return array;
+    }
+
 
 }
--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,15 +1,34 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.Before;
+import org.junit.Test;
+import static  org.junit.Assert.*;
+
+import java.io.FileReader;
 
 /**
  *
  */
 public class ParserTest {
 
-    Parser parser = new Parser();
+    private final Parser parser = new Parser();
+    private final ObjectMapper mapper = new ObjectMapper();
+    JsonNode before;
+    JsonNode after;
 
     @Before
+    @SneakyThrows
     public void setup() {
         // TODO Load in test data from before.json and after.json
+
+        before = mapper.readTree(new FileReader("src/test/resources/before.json"));
+        after = mapper.readTree(new FileReader("src/test/resources/after.json"));
+    }
+
+    @Test
+    public void parseJsonAndFindDifferencesInMeta(){
+
     }
 
     // TODO Define tests here
